09a50d824fe26f3c07871bd4765e27555bfb7ec1
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2025-01-16 09:33:19 -0500

    adds spr refactor code to apply that principle

--- design-principles/solid/srp.py
+++ design-principles/solid/srp.py
@@ -17,10 +17,10 @@
 
 from dataclasses import dataclass
 
-
 @dataclass
 class PaymentProcessor:
     def process_transaction(self, customer_data, payment_data):
+        # 1. Validation Responsibility
         if not customer_data.get("name"):
             print("Invalid customer data: missing name")
             raise ValueError("Invalid customer data: missing name")
@@ -33,6 +33,7 @@ class PaymentProcessor:
             print("Invalid payment data")
             raise ValueError("Invalid payment data")
 
+				#2. Payment Responsibility
         try:
             charge = {
                 "amount": payment_data["amount"],
@@ -46,6 +47,7 @@ class PaymentProcessor:
             print("Payment failed:", e)
             raise e
 
+				# 3. Notification Responsibility
         if "email" in customer_data["contact_info"]:
             # import smtplib
             from email.mime.text import MIMEText
@@ -71,13 +73,121 @@ class PaymentProcessor:
             print("No valid contact information for notification")
             return charge
 
+				# 4. Logging Responsibility
         with open("transactions.log", "a") as log_file:
             log_file.write(f"{customer_data['name']} paid {payment_data['amount']}\n")
             log_file.write(f"Payment status: {charge['status']}\n")
 
         return charge
+				# This class violates the Single Responsibility Principle because as you can see, it has multiple responsibilities.
+                
+### -- Refactor previous class in order to apply the Single Responsibility Principle -- ###
+
+# Validation Responsibility
+@dataclass
+class CustomerValidator:
+		def validate(self, customer_data):
+				if not customer_data.get("name"):
+						print("Invalid customer data: missing name")
+						raise ValueError("Invalid customer data: missing name")
+
+				if not customer_data.get("contact_info"):
+						print("Invalid customer data: missing contact info")
+						raise ValueError("Invalid customer data: missing contact info")
+
+# Validation Responsibility                          
+@dataclass
+class PaymentDataValidator:
+		def validate(self, payment_data):
+				if not payment_data.get("source"):
+						print("Invalid payment data")
+						raise ValueError("Invalid payment data")
+                                
+
+# Notification Responsibility
+@dataclass
+class Notifier:
+        def send_confirmation(self, customer_data):
+            if "email" in customer_data["contact_info"]:
+                # import smtplib
+                from email.mime.text import MIMEText
+
+                msg = MIMEText("Thank you for your payment.")
+                msg["Subject"] = "Payment Confirmation"
+                msg["From"] = "no-reply@example.com"
+                msg["To"] = customer_data["contact_info"]["email"]
+
+                # server = smtplib.SMTP("localhost")
+                # server.send_message(msg)
+                # server.quit()
+                print("Email sent to", customer_data["contact_info"]["email"])
+
+            elif "phone" in customer_data["contact_info"]:
+                phone_number = customer_data["contact_info"]["phone"]
+                sms_gateway = "the custom SMS Gateway"
+                print(
+                    f"send the sms using {sms_gateway}: SMS sent to {phone_number}: Thank you for your payment."
+                )
+
+# Logging Responsibility
+@dataclass
+class TransactionLogger:
+        def log(self, customer_data, payment_data, charge):
+            with open("transactions.log", "a") as log_file:
+                log_file.write(f"{customer_data['name']} paid {payment_data['amount']}\n")
+                log_file.write(f"Payment status: {charge['status']}\n")
 
 
+# Payment Responsibility
+@dataclass
+class PaymentProcessorV2:
+    def process_transaction(self, customer_data, payment_data):
+        try:
+            charge = {
+                "amount": payment_data["amount"],
+                "currency": "usd",
+                "source": payment_data["source"],
+                "description": customer_data["name"],
+                "status": "succeeded",
+            }
+            print("Payment successful")
+        except Exception as e:
+            print("Payment failed:", e)
+            raise e
+
+        return charge
+
+# Payment Service    
+@dataclass
+class PaymentService:
+        payment_processor = PaymentProcessorV2()
+        notifier = Notifier()
+        logger = TransactionLogger()
+        customer_validator: CustomerValidator()
+        payment_data_validator: PaymentDataValidator()
+
+        def process_transaction(self, customer_data, payment_data):
+                try:
+                    self.customer_validator.validate(customer_data)
+                except ValueError as e:
+                     raise e
+
+                try:
+                    self.payment_validator.validate(payment_data)
+                except ValueError as e:
+                    raise e
+
+                try:
+                    charge = self.payment_processor.process_transaction(
+                        customer_data, payment_data
+                    )
+                    self.notifier.send_confirmation(customer_data)
+                    self.logger.log(customer_data, payment_data, charge)
+                    return charge
+                except ValueError as e:
+                     raise e
+                
+                   
 if __name__ == "__main__":
     payment_processor = PaymentProcessor()
 
@@ -86,7 +196,7 @@ if __name__ == "__main__":
         "contact_info": {"email": "e@mail.com"},
     }
     customer_data_with_phone = {
-        "name": "Platzi Python",
+        "name": "Python SPR",
         "contact_info": {"phone": "1234567890"},
     }
 
