0c6542314dd778924d0fe340f2cf193b18d76b6d
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2025-01-20 11:40:10 -0500

    adds liskov sustitution principle

---
+++ design-principles/solid/d_lsp.py
@@ -0,0 +1,18 @@
+### --- Liskov Substitution Principle --- ###
+
+# Propose by Barbara Liskov in 1987
+
+# The subclases should be substitutable for their base classes
+
+# Importance:
+# - It avoids misbehavior when use subclasses.
+
+# Characteristics:
+# - The subclass should respect the contract of the base class
+# - Maintains interfaces and types compatibility
+# - It do not introduce unexpected exceptions
+
+# Benefits:
+# - Reusable code
+# - Maintain interfaces and types compatibility
+# - Reduces errors in executing time.
