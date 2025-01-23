1f0c9dfc79e93889b5e7f74ae98f7bfb3b267df5
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2025-01-23 09:52:12 -0500

    adds builder pattern, missing examples for factory, builder patterns

---
+++ design-patterns/builder.py
@@ -0,0 +1,12 @@
+### --- Builder Pattern --- ###
+
+# The Builder pattern is a creational design pattern that lets you construct complex objects step by step.
+
+# When to use:
+# - When you need to build an object in multiple steps.
+# - When you need to build different representations of an object.
+
+# How to implement:
+# - Separate the construction in a builder class.
+# - Create methods in the builder class to set the properties of the object.
+# - Create a build method that returns the final object.
